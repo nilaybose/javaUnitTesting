dc4acc8131e138387305244324ad92d656524bb4
Nilay Bose <nilay.bose@gmail.com>
2021-03-25 01:55:38 +0530

    pojo tester hash code test fiz for maps

---
+++ src/main/java/bose/edu/junit/day1/GenericProductWithHashEquals.java
@@ -0,0 +1,42 @@
+package bose.edu.junit.day1;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class GenericProductWithHashEquals {
+    private final String name;
+    private final String price;
+    private final Map<String, String> subTypes;
+
+    public GenericProductWithHashEquals(String name, String price, Map<String, String> subTypes) {
+        this.name = name;
+        this.price = price;
+        this.subTypes = subTypes;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, String> getSubTypes() {
+        return subTypes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GenericProductWithHashEquals that = (GenericProductWithHashEquals) o;
+        return Objects.equals(name, that.name)
+                && Objects.equals(price, that.price) && Objects.equals(subTypes, that.subTypes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, subTypes);
+    }
+}
--- src/test/java/bose/edu/junit/day1/TestGenericProductWithHashEquals.java
+++ src/test/java/bose/edu/junit/day1/TestGenericProductWithHashEquals.java
@@ -7,11 +7,12 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class TestGenericProduct {
+public class TestGenericProductWithHashEquals {
     @Test
     @DisplayName("Pojo setter getter test")
     public void test() {
         assertThat("Validation passes",
-                PojoTester.validate(GenericProduct.class), is(true));
+                PojoTester.validateWithEqualsAndHashcode(
+                        GenericProductWithHashEquals.class, null), is(true));
     }
 }

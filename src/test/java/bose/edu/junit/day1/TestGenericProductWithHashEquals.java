602ea7e57d61ab92a69946208181f348b45d7e0d
Nilay Bose <nilay.bose@gmail.com>
2021-03-25 02:31:09 +0530

    pojo tester hash code test fiz for maps

--- src/test/java/bose/edu/junit/day1/TestGenericProductWithHashEquals.java
+++ src/test/java/bose/edu/junit/day1/TestGenericProductWithHashEquals.java
@@ -1,8 +1,16 @@
 package bose.edu.junit.day1;
 
 import bose.edu.junit.util.PojoTester;
+import com.google.common.collect.Lists;
+import org.assertj.core.util.Sets;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.function.Supplier;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
